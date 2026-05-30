9a4c54ab84adb1a9db83192d44d61e04df12bbd4
JACE DAVID <davidmwesigwa71@gmail.com>
2026-05-30 15:35:47 +0300

    fix: improved SPA redirect for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 // GitHub Pages SPA redirect fix
-const params = new URLSearchParams(window.location.search);
-const redirect = params.get('p');
-if (redirect) {
-  window.history.replaceState(null, '', redirect);
+const redirect = sessionStorage.redirect;
+delete sessionStorage.redirect;
+if (redirect && redirect !== location.href) {
+  window.history.replaceState(null, '', redirect.replace(window.location.origin, ''));
 }
+
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
